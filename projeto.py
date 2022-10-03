2e76a99914e14e1349b17b4c07ef8ef69907d7f9
Francisco Fernandes <francisco.rmfernandes@gmail.com>
2022-10-03 13:28:52 +0100

    Justificação de Texto - funções e docstring

---
+++ projeto.py
@@ -0,0 +1,18 @@
+#1. Justificação de Texto
+
+def limpa_texto(cad):
+    """Recebe cadeia de caracteres (cad) e devolve uma cadeia correspondente à
+    remoção de caracteres ASCII brancos, substituidos pelo espaço (0x20)"""
+    
+
+
+def corta_texto(cad):
+
+
+def insere_espaço(cad, col):
+    """Recebe cadeia de caracteres e um inteiro correspondente à largura da coluna"""
+    
+def justifica_texto(cad, col):
+
+
+#2. Método de Hondt
