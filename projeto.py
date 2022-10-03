ee923a34400bac26f705b42ff95d65c0b1d0ae7f
Francisco Fernandes <francisco.rmfernandes@gmail.com>
2022-10-03 14:21:38 +0100

    Funcções principais de Justificação definidas

--- projeto.py
+++ projeto.py
@@ -3,16 +3,26 @@
 def limpa_texto(cad):
     """Recebe cadeia de caracteres (cad) e devolve uma cadeia correspondente à
     remoção de caracteres ASCII brancos, substituidos pelo espaço (0x20)"""
-    
+    cad.replace(['\t\n\v\f\r'],' ')     #Devolve a cadeia fornecida trocando os caracteres definidos entre [] pelo espaço ' '
 
-
-def corta_texto(cad):
-
+def corta_texto(cad,col):
+    cad[0:col].lstrip().rstrip()
+    cad[col]
 
 def insere_espaço(cad, col):
     """Recebe cadeia de caracteres e um inteiro correspondente à largura da coluna"""
-    
+    if cad.find(' ') == False:          #Não encontrar espaços implica a existência de uma só palavra 
+        while len(cad) < col:
+            cad.append(' ')
+
+    else:       
+        while len(cad) < col:
+            for i in cad.index(' '):    #A cada ocorrência de espaço na string fornecida, inserir um espaço, até alcançar a largura desejada
+                cad.insert(' ')
+
 def justifica_texto(cad, col):
+    if type(cad) != str or type(col) != int or (cad.find(' ') == False and len(cad) > col):
+        raise ValueError('argumentos invalidos')
+    else:
+        insere_espaço(    insere_espaço(    corta_texto(limpa_texto(cad),col),    col)    ,col)
 
-
-#2. Método de Hondt
