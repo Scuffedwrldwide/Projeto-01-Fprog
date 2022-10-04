c86c3bd529dc3d0aa2e0e0815faf2cb6c11519de
Francisco Fernandes <francisco.rmfernandes@gmail.com>
2022-10-04 12:33:43 +0100

    Função insere_espaços corretamente implementada

--- projeto.py
+++ projeto.py
@@ -3,35 +3,42 @@
 def limpa_texto(cad):
     """Recebe cadeia de caracteres (cad) e devolve uma cadeia correspondente à
     remoção de caracteres ASCII brancos, substituidos pelo espaço (0x20)"""
-    cad = cad.strip(' ')                #Remove espaços a cada lado do input
-    cad = cad.replace('\t',' ')         #Devolve a cadeia fornecida trocando os caracteres definidos  pelo espaço ' '
+    cad = cad.strip(' ')               #Remove espaços a cada lado do input
+    cad = cad.replace('\t',' ')        #Devolve a cadeia fornecida trocando os caracteres definidos  pelo espaço ' '
     cad = cad.replace('\n',' ')
     cad = cad.replace('\v',' ')
     cad = cad.replace('\f',' ')
     cad = cad.replace('\r',' ')
     while int(cad.find('  ')) > 0:
-        cad = cad.replace('  ',' ')     #Enquanto existirem sequencias de 2 ou mais espaços, substitui as sequências de 2 espaços por 1
+        cad = cad.replace('  ',' ')    #Enquanto existirem sequencias de 2 ou mais espaços, substitui as sequências de 2 espaços por 1
     return cad.strip(' ') 
 
 def corta_texto(cad,col):
     cut = ''
     if len(cad) > col:
         while cad.rfind(' ') >= col:
-            cut = cad[:cad.rfind]           #corta até ao ultimo espaço enquanto existirem espaços, e como tal palavras, fora do comprimento especificado
+            cut = cad[:cad.rfind]      #Corta até ao ultimo espaço enquanto existirem espaços, e como tal palavras, fora do comprimento especificado
         cut = cad[:cad.rfind(' ')].strip(' ')
         uncut = cad[cad.rfind(' '):].strip(' ')
     return cut, uncut   
 
 def insere_espacos(cad, col):
     """Recebe cadeia de caracteres e um inteiro correspondente à largura da coluna"""
-    if cad.find(' ') == False:          #Não encontrar espaços implica a existência de uma só palavra 
+    if cad.find(' ') == False:         #Não encontrar espaços implica a existência de uma só palavra 
         while len(cad) < col:
             cad = cad.append(' ')
 
-    else:      
+    else:
+        count = 1                                              # Número inicial de espaços
         while len(cad) < col:
+            cad = cad.replace(' '*count, ' '*(count+1))        #Aumenta ambas as instâncias de espaços
+            count +=1                                          #Atualiza a dimensão atual das sequências de espaços
+        if len(cad) > col:
+            cad = cad.replace(' '*count, ' '*(count-1))        #Reduz ambas as sequências em um espaço
+            cad = cad.replace(' '*(count-1), ' '*(count), 1)   #permitindo que se aumente apenas uma das sequencias por 1 espaço
+
             
-    
+        
     return cad
 
 def justifica_texto(cad, col):
