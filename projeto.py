6117e95ed6f421b6c234781a7a1829750e1d51b5
Francisco Fernandes <francisco.rmfernandes@gmail.com>
2022-10-07 17:30:26 +0100

    Remoção finaliza espaços, docstring, smaller

--- projeto.py
+++ projeto.py
@@ -12,11 +12,14 @@ def limpa_texto(cad):
     cad = cad.replace('\v',' ')
     cad = cad.replace('\f',' ')
     cad = cad.replace('\r',' ')
+    cad = cad.replace('  ',' ')
     while int(cad.find('  ')) > 0:
         cad = cad.replace('  ',' ')    #Enquanto existirem sequencias de 2 ou mais espaços, substitui as sequências de 2 espaços por 1
     return cad.strip(' ') 
 
 def corta_texto(cad,col):
+    """Limita cadeias de caracteres de acordo com a largura fornecida.
+    Devolve duas cadeias, a cadeia cortada e a sobrante cadeia inalterada"""
     cut = cad
     uncut = ''
     if len(cad) > col:
@@ -24,16 +27,11 @@ def corta_texto(cad,col):
         cut = cut[:cut.rfind(' ')].strip(' ')    #Limita a cadeia cortada até ao ultimo espaço eliminando palavras incompletas
         uncut = cad[len(cut):].strip(' ')        #A restante cadeia resume a após o ultimo caractér presente na cadeia cortada
     return cut, uncut      
-def finaliza_espaços(cad ,col):
-    """Adiciona espaços ao final de uma cadeia de caracteres"""
-    while len(cad) < col:
-            cad += (' ')
-    return cad
 
 def insere_espacos(cad, col):
     """Recebe cadeia de caracteres e um inteiro correspondente à largura da coluna"""
     if cad.find(' ') == -1:                                    #Não encontrar espaços implica a existência de uma só palavra 
-        return finaliza_espaços(cad ,col)
+        return cad.ljust(col,' ')
         
     else:
         count = 1                                              # Número inicial de espaços
@@ -41,22 +39,22 @@ def insere_espacos(cad, col):
             cad = cad.replace(' '*count, ' '*(count+1))        #Aumenta ambas as instâncias de espaços
             count +=1                                          #Atualiza a dimensão atual das sequências de espaços
         while len(cad) > col:
-            cad = cad.replace(' '*count, ' '*(count-1))        #Reduz todas as sequências em um espaço
+            cad = cad.replace(' '*count, ' '*(count-1))                   #Reduz todas as sequências em um espaço
             cad = cad.replace(' '*(count-1), ' '*(count), col-len(cad))   #permitindo que se aumente cada uma das sequencias por 1 espaço   
     return cad
 
 def justifica_texto(cad, col):
+    """Recebe cadeia de caracteres e um inteiro correspondente à largura da coluna.
+    Aceita tuplos."""
     if type(cad) != str or type(col) != int or col < 1 or (cad.find(' ') == -1 and len(cad)>col) or len(cad) == 0:
-        raise ValueError('justifica texto: argumentos invalidos')    #Argumentos de tipo errado, cadeia de caracteres nula ou ininterruptamente superior à largura de coluna
+        raise ValueError('justifica_texto: argumentos invalidos')              #Argumentos de tipo errado, cadeia de caracteres nula ou ininterruptamente superior à largura de coluna
     else:
-        lines = len(cad)//col           #Prevê o número de linhas necessárias para a justificação
-        next = limpa_texto(cad)         #Variavel que guarda o texto restante
+        lines = len(cad)//col                                                  #Prevê o número de linhas necessárias para a justificação
+        next = limpa_texto(cad)                                                #Variavel que guarda o texto restante
         cad = []
-        for i in range(lines-1):   
-            cad = cad + [str(insere_espacos(corta_texto(next,col)[0], col))]
-            next = str(corta_texto(next,col)[1])
-            lines -= 1
-        cad = cad + [finaliza_espaços(next,col)]                              
+        for i in range(lines-1):                                               #Itera sobre cada linha, excepto a ultima, que requere um procedimento particular   
+            cad.append(str(insere_espacos(corta_texto(next,col)[0], col)))     #Adiciona à lista a cadeia cortada e espaçada
+            next = str(corta_texto(next,col)[1])                               #Prepara a cadeia inalterada para o proximo ciclo
+        cad.append(next.ljust(col,' '))                                        #A ultima linha de cada texto é unicamente justificada à esquerda
         return tuple(cad)
 
-print(insere_espacos('Fundamentos', 15))
